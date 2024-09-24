117a9b2e308d9b1c846659df37d0787aee50b5a1
Mohammed Siddiqui <zia.investing@gmail.com>
2024-09-23 20:15:36 -0400

    added modal and buttons

--- src/components/employee.js
+++ src/components/employee.js
@@ -1,6 +1,8 @@
+import EditEmployee from "./EditEmployee"
+
+
 function Employee (props) {
     return (
-    <div>
         <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
         <img className="object-cover rounded=full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt="" />
         <div className="text-center space-y-2 sm:text-left">
@@ -12,10 +14,10 @@ function Employee (props) {
                 {props.role}
             </p>
             </div>
-                <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+
+            <EditEmployee />
             </div>
         </div>
-    </div>
 )}
 
 export default Employee
